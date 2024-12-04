1f18dd6700011094c963d2f7c2c7eb1c7edff199
Suraj Goel <24977786+surajgoel5@users.noreply.github.com>
2024-12-04 21:15:37 +0000

    Create cli.py

---
+++ src/remopy/cli.py
@@ -0,0 +1,16 @@
+# cli.py
+import argparse
+from .server import Server
+
+def main():
+    parser = argparse.ArgumentParser(description="Run the Server.")
+    parser.add_argument("--ip", type=str, default="0.0.0.0", help="IP address to bind to")
+    parser.add_argument("--job_port", type=int, default=5555, help="Port to bind to")
+    parser.add_argument("--pub_port", type=int, default=5556, help="Port to bind to")
+  
+
+    args = parser.parse_args()
+
+    # Instantiate and run the server
+    server = Server(ip=args.ip, job_port=args.job_port,pub_port=args.pub_port)
+    server.run()
