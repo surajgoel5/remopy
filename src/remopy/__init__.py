7423b413b7fa6ad06e0928b53e6a8544b0bc6726
Suraj Goel <goel.suraj5@gmail.com>
2024-12-04 00:10:49 +0000

    Update __init__.py

--- src/remopy/__init__.py
+++ src/remopy/__init__.py
@@ -4,5 +4,5 @@ __author__ = """Suraj Goel"""
 __email__ = 'goel.suraj5@gmail.com'
 __version__ = '0.1.0'
 
-from Client import Client
-from Server import Server
+from .Client import Client
+from .Server import Server
